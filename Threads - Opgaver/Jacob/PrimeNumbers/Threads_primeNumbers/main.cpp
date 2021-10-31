50f9d94fc476f43506691f5b1edff5153b3ad440
Jacob Brengdahl Larsen <jlarsen2107@gmail.com>
2021-10-31 11:47:58 +0100

    pthread lort

--- Threads - Opgaver/Jacob/PrimeNumbers/Threads_primeNumbers/main.cpp
+++ Threads - Opgaver/Jacob/PrimeNumbers/Threads_primeNumbers/main.cpp
@@ -1,4 +1,5 @@
 #include <thread>
+#include <pthread.h>
 #include <chrono>
 #include <iostream>
 #include <random>
@@ -11,54 +12,96 @@ int primes[] = {1,2,3,5,7,11,13,17,19,23,29,31,37,41,43,47};
 
 mutex arr_mutex;
 
-bool notPrime(int num){
-    return !(find(begin(primes), end(primes), num) != end(primes));
+bool isPrime(int num){
+    return (find(begin(primes), end(primes), num) != end(primes));
 }
 
-bool isSubset(int arr1[], int arr2[], int m, int n) {
-    int i = 0;
+bool carryon(int arr[], int arrSize) {
+    // Are all prime numbers in the array, return false, else true
+    int primesSize = sizeof(primes)/sizeof(primes[0]);
     int j = 0;
-    for (i = 0; i < n; i++)
-    {
-        for (j = 0; j < m; j++)
-        {
-            if(arr2[i] == arr1[j])
+    for (int i = 0; i < primesSize; i++) {
+        for (j = 0; j < arrSize; j++) {
+            if (primes[i] == arr[j]) {
                 break;
+            }
         }
-
-        if (j == m)
-            return false;
+        if (j == arrSize) {
+            return true;
+        }
     }
-    return true;
+    return false;
 }
 
+void insertNum(int arr[], int arrSize) {
+    std::uniform_int_distribution<int> distribution(1, 50);
+    std::random_device rd;
+    std::default_random_engine generator(rd());
 
-bool carryon(int arr[]) {
-    return isSubset(arr, primes, sizeof(&arr)/sizeof(arr[0]), sizeof(&primes)/sizeof(primes[0]));
+    for (int i = 0; i < arrSize; i++) {
+        if (arr[i] == 0) {
+            arr[i] = distribution(generator);
+            break;
+        }
+    }
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
 }
 
-void insertNum(int arr[]) {
-    /*
-     * Produceren genererer tilfældige heltal mellem 1 og 50, som placeres i en tabel/collection.
-     * Hver gang den har indsat et nyt tal sover den i 100 millisekunder.
-    */
-
+void removeNums(int arr[], int arrSize) {
+    for (int i = 0; i < arrSize; i++) {
+        if (!isPrime(arr[i])) {
+            arr[i] = 0;
+        }
+    }
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
 }
 
-void removeNums(int arr[]) {
-
+int usedElems(int arr[], int arrSize) {
+    int count = 0;
+    for (int i = 0; i < arrSize; i++) {
+        if (arr[i] != 0) {
+            count += 1;
+        }
+    }
+    return count;
 }
-
 
 int main()
 {
     int arr[100] = {0};
-    thread producer{insertNum, arr};
-    thread consumer{removeNums, arr};
+    int arrSize = sizeof(arr)/sizeof(arr[0]);
+
+    thread producer{insertNum, arr, arrSize};
+    thread consumer{removeNums, arr, arrSize};
 
     producer.join();
     consumer.join();
 
-    cout << "sze: " << endl;
+    cout << "size: " << usedElems(arr, arrSize) << endl;
+
+
+    //cout << "notPrime: " << isPrime(4) << endl;
+
+    /*
+    int arr[] = {1,2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,4};
+    int arrSize = sizeof(arr)/sizeof(arr[0]);
+
+    //insertNum(arr, arrSize);
+
+    cout << "arr: " << endl;
+    for (size_t i = 0; i < arrSize; i++) {
+        std::cout << arr[i] << ' ';
+    }
+    cout << endl;
+
+    removeNums(arr, arrSize);
+
+    cout << "arr: " << endl;
+    for (size_t i = 0; i < arrSize; i++) {
+        std::cout << arr[i] << ' ';
+    }
+    cout << endl;
+    */
+
     return 0;
 }
