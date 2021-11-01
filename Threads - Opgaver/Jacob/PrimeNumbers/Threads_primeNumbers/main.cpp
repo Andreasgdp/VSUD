fc0f646c6d7163d621168ec15becc2ae92c87652
Jacob Brengdahl Larsen <jlarsen2107@gmail.com>
2021-11-01 13:05:23 +0100

    Mutex work too :D:D:D:D:D:D:D:D:D

--- Threads - Opgaver/Jacob/PrimeNumbers/Threads_primeNumbers/main.cpp
+++ Threads - Opgaver/Jacob/PrimeNumbers/Threads_primeNumbers/main.cpp
@@ -16,7 +16,7 @@ void printArray(int arr[], int arrSize, string text) {
     for (size_t i = 0; i < arrSize; i++) {
         std::cout << arr[i] << ' ';
     }
-    cout << endl;
+    cout << endl << endl;
 }
 
 bool isPrime(int num){
@@ -28,11 +28,13 @@ bool carryon(int arr[], int arrSize) {
     int primesSize = sizeof(primes)/sizeof(primes[0]);
     int j = 0;
     for (int i = 0; i < primesSize; i++) {
+        arr_mutex.lock();
         for (j = 0; j < arrSize; j++) {
             if (primes[i] == arr[j]) {
                 break;
             }
         }
+        arr_mutex.unlock();
         if (j == arrSize) {
             return true;
         }
@@ -46,7 +48,7 @@ void insertNum(int arr[], int arrSize) {
     std::default_random_engine generator(rd());
 
     while (carryon(arr, arrSize)) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        arr_mutex.lock();
         for (int i = 0; i < arrSize; i++) {
             if (arr[i] == 0) {
                 arr[i] = distribution(generator);
@@ -54,18 +56,22 @@ void insertNum(int arr[], int arrSize) {
             }
         }
         printArray(arr, arrSize, "arr (insert): ");
+        arr_mutex.unlock();
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 }
 
 void removeNums(int arr[], int arrSize) {
     while (carryon(arr, arrSize)) {
         std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        arr_mutex.lock();
         for (int i = 0; i < arrSize; i++) {
             if (!isPrime(arr[i])) {
                 arr[i] = 0;
             }
         }
         printArray(arr, arrSize, "arr (remove): ");
+        arr_mutex.unlock();
     }
 }
 
