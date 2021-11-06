eb245cb1b73c25afaace01845363fd274803d90b
Jacob Brengdahl Larsen <jlarsen2107@gmail.com>
2021-11-06 16:19:23 +0100

    Why "show (*)(*)" not work, huh?

--- DB og tråde - Obligatroisk opgave 2/Jacob/Portefolje_2_DB_og_Traade/main.cpp
+++ DB og tråde - Obligatroisk opgave 2/Jacob/Portefolje_2_DB_og_Traade/main.cpp
@@ -9,15 +9,17 @@
 #include <QSqlDriver>
 
 #include "NumberPlateGenerator.h"
+#include "Database.h"
 
 using namespace std;
 
 mutex mtx;
 
-string modeler[] = {"Audi","BMW","VW","Mercedes","Volvo"};
+string modeller[] = {"Audi","BMW","VW","Mercedes","Volvo"};
 string ejerer[] = {"Jens","Peter","Knud","Hans","Egon"};
 
 NumberPlateGenerator NumberPlateGenerator;
+Database db;
 
 std::uniform_int_distribution<int> distribution(0, 4);
 std::uniform_int_distribution<int> aargang(1900, 2021);
@@ -25,16 +27,10 @@ std::random_device rd;
 std::default_random_engine generator(rd());
 
 void generateData(char* numberPlate) {
-    mtx.lock();
     for (int i = 0; i < 20000; i++) {
         NumberPlateGenerator.generate(numberPlate);
-        cout << numberPlate << " " << modeler[distribution(generator)] << " "
-             << aargang(generator) << " " << ejerer[distribution(generator)] << endl;
-
-        //INSERT INTO bil (registreringsnr, model, aargang, ejer)
-        //VALUES (numberPlate, modeler[distribution(generator)], aargang(generator), ejerer[distribution(generator)]);
+        db.insertCar(numberPlate, modeller[distribution(generator)], aargang(generator), ejerer[distribution(generator)]);
     }
-    mtx.unlock();
 }
 
 void addCar(char* numberPlate) {
@@ -52,9 +48,11 @@ void reRegisterCar(char* numberPlate) {
 
 int main()
 {
-    char* numberPlate = new char[7];
+    db.createDatabase();
 
-    generateData(numberPlate);
+    char* numberPlate = new char[7];
+    if (db.isEmpty())
+        generateData(numberPlate);
 
     thread adder{addCar, numberPlate};
     thread deleter{removeCar};
@@ -63,11 +61,6 @@ int main()
     adder.join();
     deleter.join();
     reRegister.join();
-
-    //    for (int i = 0; i < 200100; i++) {
-    //        NumberPlateGenerator.generate(numberPlate);
-    //        cout << numberPlate << endl;
-    //    }
 
     return 0;
 }
